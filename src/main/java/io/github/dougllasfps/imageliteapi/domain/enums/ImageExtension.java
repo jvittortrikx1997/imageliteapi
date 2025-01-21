4e306e3b008cb7dcad27fc0d09840a344be3d20f
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2025-01-20 21:50:13 -0300

    feat: getImageId

--- src/main/java/io/github/dougllasfps/imageliteapi/domain/enums/ImageExtension.java
+++ src/main/java/io/github/dougllasfps/imageliteapi/domain/enums/ImageExtension.java
@@ -1,5 +1,6 @@
 package io.github.dougllasfps.imageliteapi.domain.enums;
 
+import lombok.Getter;
 import org.springframework.http.MediaType;
 
 import java.awt.*;
@@ -9,6 +10,8 @@ public enum ImageExtension {
     PNG(MediaType.IMAGE_PNG),
     GIF(MediaType.IMAGE_GIF),
     JPEG(MediaType.IMAGE_JPEG);
+
+    @Getter
 
     private MediaType mediaType;
 
--- src/main/java/io/github/dougllasfps/imageliteapi/infra/repository/ImageRepository.java
+++ src/main/java/io/github/dougllasfps/imageliteapi/infra/repository/ImageRepository.java
@@ -3,5 +3,8 @@ package io.github.dougllasfps.imageliteapi.infra.repository;
 import io.github.dougllasfps.imageliteapi.domain.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ImageRepository extends JpaRepository<Image, String> {
+
 }
