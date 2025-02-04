72370fb42a439688d9889840033fd34d82134fa5
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2025-02-03 22:07:15 -0300

    feat:dto

--- src/main/java/io/github/dougllasfps/imageliteapi/infra/repository/ImageRepository.java
+++ src/main/java/io/github/dougllasfps/imageliteapi/infra/repository/ImageRepository.java
@@ -1,10 +1,17 @@
 package io.github.dougllasfps.imageliteapi.infra.repository;
 
 import io.github.dougllasfps.imageliteapi.domain.entity.Image;
+import io.github.dougllasfps.imageliteapi.domain.enums.ImageExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface ImageRepository extends JpaRepository<Image, String> {
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
 
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
+
+        return findAll();
+    }
 }
